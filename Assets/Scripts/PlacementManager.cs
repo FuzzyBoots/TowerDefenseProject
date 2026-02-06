ca294e6e21a4dc67a84dcd432b4a72fe543aed8f
Sean Duggan <sean.duggan@gmail.com>
2026-02-05 20:08:02 -0500

    Placement updates

--- Assets/Scripts/PlacementManager.cs
+++ Assets/Scripts/PlacementManager.cs
@@ -1,13 +1,16 @@
 using Cinemachine;
+using nTools.PrefabPainter;
 using System;
 using System.Collections.Specialized;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class PlacementManager : MonoBehaviour
 {
     [SerializeField] GameObject _currentPrefab;
     [SerializeField] LayerMask _placementLayer;
+    private bool _overPlacement;
 
     private void Start()
     {
@@ -26,36 +29,76 @@ public class PlacementManager : MonoBehaviour
             Debug.DrawRay(ray.origin, ray.direction * 100f, Color.red);
 
             RaycastHit raycastHit;
-            if (Physics.Raycast(ray, out raycastHit, 100f, _placementLayer))
+            if (Physics.Raycast(ray, out raycastHit, 100f, _placementLayer, QueryTriggerInteraction.Collide))
             {
                 Collider targetCollider = raycastHit.collider;
-                float topY = targetCollider.bounds.max.y;
+                // Different behavior for a Placement Point or a Wall/Floor
+                if (targetCollider.gameObject.layer == LayerMask.NameToLayer("Placement"))
+                {
+                    Debug.Log("Placement at " + targetCollider.transform.position);
+                    _overPlacement = true;
+                    _currentPrefab.transform.position = targetCollider.transform.position;
+                } else
+                {
+                    _overPlacement = false;
+                    float topY = targetCollider.bounds.max.y;
 
-                turretPosition = new(raycastHit.point.x, topY, raycastHit.point.z);
-                _currentPrefab.transform.position = turretPosition;
-                Debug.Log("Hit " + raycastHit.collider.name);
+                    turretPosition = new(raycastHit.point.x, topY, raycastHit.point.z);
+                    _currentPrefab.transform.position = turretPosition;
+                }                    
             }
         }
 
-        if (Mouse.current.leftButton.wasPressedThisFrame)
+        if (Mouse.current.leftButton.wasPressedThisFrame && !IsCursorOverUI())
         {
-            // Place the turret at turretPosition and activate it
+            Debug.Log("Clicked");
+            if (_overPlacement)
+            {
+                Debug.Log("Placed?");
+                // Place the turret at turretPosition and activate it
+                // We'll assign a new copy to the one we're moving... will this work?
+                _currentPrefab = Instantiate(_currentPrefab);
+            }
         }
 
         if (Mouse.current.rightButton.wasPressedThisFrame)
         {
+            Debug.Log("Canceled");
             CancelSetting();
         }
     }
 
     public void SetPrefab(GameObject prefab)
     {
+        if (_currentPrefab != null)
+        {
+            Destroy(_currentPrefab);
+        }
+
         _currentPrefab = Instantiate(prefab);
-        Debug.Log("Instantiated Prefab " + prefab.name);
     }
 
     public void CancelSetting()
     {
-        Destroy(_currentPrefab);
+        if (_currentPrefab)
+        {
+            Destroy(_currentPrefab);
+        }
+    }
+
+    private bool IsCursorOverUI()
+    {
+        // For mouse input: no parameters needed
+        if (EventSystem.current.IsPointerOverGameObject())
+            return true;
+
+        // For touch input: pass the touch ID (0 for single touch)
+        foreach (Touch touch in Input.touches)
+        {
+            if (EventSystem.current.IsPointerOverGameObject(touch.fingerId))
+                return true;
+        }
+
+        return false;
     }
 }
